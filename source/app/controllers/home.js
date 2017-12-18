5538b778782b452791fa5d6c338d882313eeea38
Jelle van den Berg <jellevdberg1996@gmail.com>
2017-12-18 08:45:13 +0100

    Chaneg website

--- source/app/controllers/home.js
+++ source/app/controllers/home.js
@@ -5,8 +5,8 @@
 
 angular.module('controllers').controller('HomeController', ['$scope', 'CryptoService', '$timeout', '$location', function ($scope, CryptoService, $timeout, $location) {
     $scope.reload = reload;
-    $scope.getEthHistory = getEthHistory;
-    $scope.getBtcHistory = getBtcHistory;
+    // $scope.getEthHistory = getEthHistory;
+    // $scope.getBtcHistory = getBtcHistory;
 
     init();
 
@@ -18,8 +18,8 @@ angular.module('controllers').controller('HomeController', ['$scope', 'CryptoSer
         $scope.$broadcast('toggle-partial');
 
         $scope.reload();
-        $scope.getBtcHistory();
-        $scope.getEthHistory();
+        // $scope.getBtcHistory();
+        // $scope.getEthHistory();
     }
 
     function reload() {
@@ -77,48 +77,48 @@ angular.module('controllers').controller('HomeController', ['$scope', 'CryptoSer
         }, 15000);
     }
 
-    function getEthHistory() {
-        $scope.eth_data = CryptoService.eth_short();
-
-        CryptoService.eth_1m().then(function (data) {
-            $scope.eth_1m = data.data;
-            $scope.eth_1m.name = '1m';
-            $scope.eth_1m.order = 9;
-            $scope.eth_data.push($scope.eth_1m);
-        });
-
-
-        $scope.eth_array = objToArray($scope.eth_data);
-        console.log('eth long', $scope.eth_array);
-
-        $timeout(function () {
-            $scope.getEthHistory();
-        }, 360000);
-    }
-
-    function getBtcHistory() {
-        $scope.btc_data = CryptoService.btc_short();
-
-        CryptoService.btc_1m().then(function (data) {
-            $scope.btc_1m = data.data;
-            $scope.btc_1m.name = '1m';
-            $scope.btc_1m.order = 9;
-            $scope.btc_data.push($scope.btc_1m);
-        });
-
-        $scope.btc_array = objToArray($scope.btc_data);
-        $timeout(function () {
-            $scope.getBtcHistory();
-        }, 360000);
-    }
-
-    function objToArray(obj) {
-        var output = [];
-        for (var item in obj) {
-            console.log('item in obj', item);
-            output.push(item);
-        }
-        return output;
-    }
+    // function getEthHistory() {
+    //     $scope.eth_data = CryptoService.eth_short();
+    //
+    //     CryptoService.eth_1m().then(function (data) {
+    //         $scope.eth_1m = data.data;
+    //         $scope.eth_1m.name = '1m';
+    //         $scope.eth_1m.order = 9;
+    //         $scope.eth_data.push($scope.eth_1m);
+    //     });
+    //
+    //
+    //     $scope.eth_array = objToArray($scope.eth_data);
+    //     console.log('eth long', $scope.eth_array);
+    //
+    //     $timeout(function () {
+    //         $scope.getEthHistory();
+    //     }, 360000);
+    // }
+    //
+    // function getBtcHistory() {
+    //     $scope.btc_data = CryptoService.btc_short();
+    //
+    //     CryptoService.btc_1m().then(function (data) {
+    //         $scope.btc_1m = data.data;
+    //         $scope.btc_1m.name = '1m';
+    //         $scope.btc_1m.order = 9;
+    //         $scope.btc_data.push($scope.btc_1m);
+    //     });
+    //
+    //     $scope.btc_array = objToArray($scope.btc_data);
+    //     $timeout(function () {
+    //         $scope.getBtcHistory();
+    //     }, 360000);
+    // }
+    //
+    // function objToArray(obj) {
+    //     var output = [];
+    //     for (var item in obj) {
+    //         console.log('item in obj', item);
+    //         output.push(item);
+    //     }
+    //     return output;
+    // }
 
 }]);
--- source/app/services/crypto-service.js
+++ source/app/services/crypto-service.js
@@ -63,132 +63,132 @@ var cc = 'https://min-api.cryptocompare.com/data/';
 
             return deferred.promise;
         },
-         eth_1m: function () {
-            var deferred = $q.defer();
-            var curr = new Date(); // get current date
-            var last = (curr.getDate() - curr.getMonth()) - 1;
-            var date = formatDate(new Date(curr.setDate(last)));
-            console.log('date', date);
-            $http.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot', {params: {'date': date}}).success(function (data) {
-
-                deferred.resolve(data);
-
-            }).error(function () {
-
-                deferred.reject("not found");
-            });
-            return deferred.promise;
-        },
-        eth_24h: function () {
-            var deferred = $q.defer();
-            var curr = new Date(); // get current date
-            var last = (curr.getDate() - curr.getDay()) - 1; // First day is the day of the month - the day of the week
-            var date = formatDate(new Date(curr.setDate(last)));
-            $http.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot', {params: {'date': date}}).success(function (data) {
-
-                deferred.resolve(data);
-
-            }).error(function () {
-
-                deferred.reject("not found");
-            });
-            return deferred.promise;
-        },
-        eth_7d: function () {
-            var deferred = $q.defer();
-            var curr = new Date(); // get current date
-            var last = (curr.getDate() - curr.getDay()) - 7; // First day is the day of the month - the day of the week
-            var date = formatDate(new Date(curr.setDate(last)));
-            $http.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot', {params: {'date': date}}).success(function (data) {
-
-                deferred.resolve(data);
-
-            }).error(function () {
-
-                deferred.reject("not found");
-            });
-            return deferred.promise;
-        },
-        btc_1m: function () {
-            var deferred = $q.defer();
-            var curr = new Date(); // get current date
-            var last = (curr.getDate() - curr.getMonth()) - 1;
-            var date = formatDate(new Date(curr.setDate(last)));
-            console.log('date', date);
-            $http.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot', {params: {'date': date}}).success(function (data) {
-
-                deferred.resolve(data);
-
-            }).error(function () {
-
-                deferred.reject("not found");
-            });
-            return deferred.promise;
-        },
-        btc_24h: function () {
-            var deferred = $q.defer();
-            var curr = new Date(); // get current date
-            var last = (curr.getDate() - curr.getDay()) - 1; // First day is the day of the month - the day of the week
-            var date = formatDate(new Date(curr.setDate(last)));
-            $http.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot', {params: {'date': date}}).success(function (data) {
-
-                deferred.resolve(data);
-
-            }).error(function () {
-
-                deferred.reject("not found");
-            });
-            return deferred.promise;
-        },
-        btc_7d: function () {
-            var deferred = $q.defer();
-            var curr = new Date(); // get current date
-            var last = (curr.getDate() - curr.getDay()) - 7; // First day is the day of the month - the day of the week
-            var date = formatDate(new Date(curr.setDate(last)));
-            $http.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot', {params: {'date': date}}).success(function (data) {
-
-                deferred.resolve(data);
-
-            }).error(function () {
-
-                deferred.reject("not found");
-            });
-            return deferred.promise;
-        },
-
-        eth_short: function () {
-            var eth_short = [];
-            $http.get(cc + 'histohour', {params: {fsym: 'ETH', tsym: 'EUR'}}).then(function(data) {
-                var result = data.data.Data;
-                // console.log('result', new Date(result[0].time));
-                // console.log('result', new Date(result[168].time * 1000));
-                eth_short.push(eth_7d = {'amount': result[0].close, 'name': '7d', 'order': 8});
-                eth_short.push(eth_3d = {'amount': result[72].close, 'name': '3d', 'order': 7});
-                eth_short.push(eth_1d = {'amount': result[144].close, 'name': '1d', 'order': 6});
-                eth_short.push(eth_12h = {'amount': result[166].close, 'name': '12h','order': 5});
-                eth_short.push(eth_6h = {'amount': result[162].close, 'name': '6h','order': 4});
-                eth_short.push(eth_3h = {'amount': result[166].close, 'name': '3h', 'order': 3});
-                eth_short.push(eth_2h = {'amount': result[167].close, 'name': '2h', 'order': 2});
-                eth_short.push(eth_1h = {'amount': result[168].close, 'name': '1h', 'order': 1});
-            });
-            return eth_short;
-        },
-        btc_short: function () {
-            var btc_short = [];
-            $http.get(cc + 'histohour', {params: {fsym: 'BTC', tsym: 'EUR'}}).then(function(data){
-                var result = data.data.Data;
-                // console.log('result', new Date(result[168].time * 1000));
-                btc_short.push(btc_7d = {'amount': result[0].close, 'name': '7d', 'order': 8});
-                btc_short.push(btc_3d = {'amount': result[72].close, 'name': '3d','order': 7});
-                btc_short.push(btc_1d = {'amount': result[144].close, 'name': '1d', 'order': 6});
-                btc_short.push(btc_12h = {'amount': result[166].close, 'name': '12h', 'order': 5});
-                btc_short.push(btc_6h = {'amount': result[162].close, 'name': '6h', 'order': 4});
-                btc_short.push(btc_3h = {'amount': result[166].close, 'name': '3h', 'order': 3});
-                btc_short.push(btc_2h = {'amount': result[167].close, 'name': '2h', 'order': 2});
-                btc_short.push(btc_1h = {'amount': result[168].close, 'name': '1h', 'order': 1});
-            });
-            return btc_short;
-        }
+        //  eth_1m: function () {
+        //     var deferred = $q.defer();
+        //     var curr = new Date(); // get current date
+        //     var last = (curr.getDate() - curr.getMonth()) - 1;
+        //     var date = formatDate(new Date(curr.setDate(last)));
+        //     console.log('date', date);
+        //     $http.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot', {params: {'date': date}}).success(function (data) {
+        //
+        //         deferred.resolve(data);
+        //
+        //     }).error(function () {
+        //
+        //         deferred.reject("not found");
+        //     });
+        //     return deferred.promise;
+        // },
+        // eth_24h: function () {
+        //     var deferred = $q.defer();
+        //     var curr = new Date(); // get current date
+        //     var last = (curr.getDate() - curr.getDay()) - 1; // First day is the day of the month - the day of the week
+        //     var date = formatDate(new Date(curr.setDate(last)));
+        //     $http.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot', {params: {'date': date}}).success(function (data) {
+        //
+        //         deferred.resolve(data);
+        //
+        //     }).error(function () {
+        //
+        //         deferred.reject("not found");
+        //     });
+        //     return deferred.promise;
+        // },
+        // eth_7d: function () {
+        //     var deferred = $q.defer();
+        //     var curr = new Date(); // get current date
+        //     var last = (curr.getDate() - curr.getDay()) - 7; // First day is the day of the month - the day of the week
+        //     var date = formatDate(new Date(curr.setDate(last)));
+        //     $http.get('https://api.coinbase.com/v2/prices/ETH-EUR/spot', {params: {'date': date}}).success(function (data) {
+        //
+        //         deferred.resolve(data);
+        //
+        //     }).error(function () {
+        //
+        //         deferred.reject("not found");
+        //     });
+        //     return deferred.promise;
+        // },
+        // btc_1m: function () {
+        //     var deferred = $q.defer();
+        //     var curr = new Date(); // get current date
+        //     var last = (curr.getDate() - curr.getMonth()) - 1;
+        //     var date = formatDate(new Date(curr.setDate(last)));
+        //     console.log('date', date);
+        //     $http.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot', {params: {'date': date}}).success(function (data) {
+        //
+        //         deferred.resolve(data);
+        //
+        //     }).error(function () {
+        //
+        //         deferred.reject("not found");
+        //     });
+        //     return deferred.promise;
+        // },
+        // btc_24h: function () {
+        //     var deferred = $q.defer();
+        //     var curr = new Date(); // get current date
+        //     var last = (curr.getDate() - curr.getDay()) - 1; // First day is the day of the month - the day of the week
+        //     var date = formatDate(new Date(curr.setDate(last)));
+        //     $http.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot', {params: {'date': date}}).success(function (data) {
+        //
+        //         deferred.resolve(data);
+        //
+        //     }).error(function () {
+        //
+        //         deferred.reject("not found");
+        //     });
+        //     return deferred.promise;
+        // },
+        // btc_7d: function () {
+        //     var deferred = $q.defer();
+        //     var curr = new Date(); // get current date
+        //     var last = (curr.getDate() - curr.getDay()) - 7; // First day is the day of the month - the day of the week
+        //     var date = formatDate(new Date(curr.setDate(last)));
+        //     $http.get('https://api.coinbase.com/v2/prices/BTC-EUR/spot', {params: {'date': date}}).success(function (data) {
+        //
+        //         deferred.resolve(data);
+        //
+        //     }).error(function () {
+        //
+        //         deferred.reject("not found");
+        //     });
+        //     return deferred.promise;
+        // },
+        //
+        // eth_short: function () {
+        //     var eth_short = [];
+        //     $http.get(cc + 'histohour', {params: {fsym: 'ETH', tsym: 'EUR'}}).then(function(data) {
+        //         var result = data.data.Data;
+        //         // console.log('result', new Date(result[0].time));
+        //         // console.log('result', new Date(result[168].time * 1000));
+        //         eth_short.push(eth_7d = {'amount': result[0].close, 'name': '7d', 'order': 8});
+        //         eth_short.push(eth_3d = {'amount': result[72].close, 'name': '3d', 'order': 7});
+        //         eth_short.push(eth_1d = {'amount': result[144].close, 'name': '1d', 'order': 6});
+        //         eth_short.push(eth_12h = {'amount': result[166].close, 'name': '12h','order': 5});
+        //         eth_short.push(eth_6h = {'amount': result[162].close, 'name': '6h','order': 4});
+        //         eth_short.push(eth_3h = {'amount': result[166].close, 'name': '3h', 'order': 3});
+        //         eth_short.push(eth_2h = {'amount': result[167].close, 'name': '2h', 'order': 2});
+        //         eth_short.push(eth_1h = {'amount': result[168].close, 'name': '1h', 'order': 1});
+        //     });
+        //     return eth_short;
+        // },
+        // btc_short: function () {
+        //     var btc_short = [];
+        //     $http.get(cc + 'histohour', {params: {fsym: 'BTC', tsym: 'EUR'}}).then(function(data){
+        //         var result = data.data.Data;
+        //         // console.log('result', new Date(result[168].time * 1000));
+        //         btc_short.push(btc_7d = {'amount': result[0].close, 'name': '7d', 'order': 8});
+        //         btc_short.push(btc_3d = {'amount': result[72].close, 'name': '3d','order': 7});
+        //         btc_short.push(btc_1d = {'amount': result[144].close, 'name': '1d', 'order': 6});
+        //         btc_short.push(btc_12h = {'amount': result[166].close, 'name': '12h', 'order': 5});
+        //         btc_short.push(btc_6h = {'amount': result[162].close, 'name': '6h', 'order': 4});
+        //         btc_short.push(btc_3h = {'amount': result[166].close, 'name': '3h', 'order': 3});
+        //         btc_short.push(btc_2h = {'amount': result[167].close, 'name': '2h', 'order': 2});
+        //         btc_short.push(btc_1h = {'amount': result[168].close, 'name': '1h', 'order': 1});
+        //     });
+        //     return btc_short;
+        // }
 
     };
 
